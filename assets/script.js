c344201f7cf8d58519869dc541b21f6c7dd633a9
Blake H <blake.hollingsworth80@gmail.com>
2023-05-15 17:02:19 -0600

    answer button text now changes text-content

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,12 @@ var startButton = document.getElementById('startButton')
 var startScreen = document.getElementById('startScreen')
 var quizScreen = document.getElementById('quiz')
 var timerDisplay = document.getElementById('timer')
+
 var questionEl = document.getElementById('question')
+var answerEl1 = document.getElementById('answer1')
+var answerEl2 = document.getElementById('answer2')
+var answerEl3 = document.getElementById('answer3')
+var answerEl4 = document.getElementById('answer4')
 
 // timer initialization
 quizTimeAmount = 51
@@ -20,15 +25,20 @@ function timer() {
 }
 
 // function to add html elements to display Q&As
-function displayQuestion(question) {
-    questionEl.textContent = question;
+function displayElement(element, question) {
+    element.textContent = question;
 }
 
 // quiz questions
-var questions = ['Which is an example of a data type?']
+var questions = ['Which is an example of a data type?'];
+var correctAnswers = ['Boolean'];
 var i = 0;
 function startQuiz() {
-    displayQuestion(questions[i]);
+    displayElement(questionEl, questions[i]);
+    displayElement(answerEl1, correctAnswers[i]);
+    displayElement(answerEl2, correctAnswers[i]);
+    displayElement(answerEl3, correctAnswers[i]);
+    displayElement(answerEl4, correctAnswers[i]);
     i++;
 }
 
