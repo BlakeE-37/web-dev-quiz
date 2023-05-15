ba7ce78da245a51ca08675fcdcafcf61db729c5c
Blake H <blake.hollingsworth80@gmail.com>
2023-05-15 15:59:05 -0600

    changed order of javascript functions and variable declarations and added comments

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,10 @@
+// Retrieve all needed elements from the html
 var startButton = document.getElementById('startButton')
 var startScreen = document.getElementById('startScreen')
 var timerDisplay = document.getElementById('timer')
 
-startButton.addEventListener('click', initialize)
 
-function initialize() {
-    timer();
-    startScreen.setAttribute('style', 'display: none;')
-}
-
+// timer initialization
 quizTimeAmount = 51
 function timer() {
     var quizTimer = setInterval(() => {
@@ -21,3 +17,18 @@ function timer() {
         }
     }, 1000)
 }
+
+// quiz questions
+
+// function to add html elements to display Q&As
+function displayQuiz() {
+
+}
+
+function initialize() {
+    startScreen.setAttribute('style', 'display: none;');
+    displayQuiz();
+    timer();
+}
+
+startButton.addEventListener('click', initialize)
